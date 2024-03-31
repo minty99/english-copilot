f97d740d865c7a6471e26cdebebf0bcad94fcc5e
Muhwan Kim <mhkim4886@gmail.com>
2024-04-01 00:11:33 +0900

    PoC: now it works

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,74 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import OpenAI from "openai";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let channel = vscode.window.createOutputChannel("English Copilot");
+	channel.appendLine("English Copilot activated")
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "english-copilot" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('english-copilot.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('english-copilot.naturalize', async () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from English Copilot!');
+		let config = vscode.workspace.getConfiguration('english-copilot');
+		let openAiApiKey = config.get('openAiApiKey');
+		if (!openAiApiKey) {
+			vscode.window.showErrorMessage('Please set your OpenAI API key in the settings');
+			return;
+		}
+		let openAiModelName = config.get('openAiModelName');
+		const openai = new OpenAI({ apiKey: openAiApiKey as string });
+
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const selection = editor.selection;
+			const selectedText = editor.document.getText(selection);
+			console.log('Selected text:', selectedText);
+			if (selectedText) {
+				// Do something with the selected text
+				const response = await openai.chat.completions.create({
+					messages: [{
+						role: "system",
+						content: `
+You are an English language assistant designed to help software engineers who find it challenging to write comments in English.
+Your role is to assist them in improving the clarity and fluency of their comments.
+When provided with a comment, your task is to translate or convert its contents into natural and fluent English.
+Correct any grammatical errors and ensure that the output text uses commonly understood English terminology.
+Only output the resulting text without any additional explanations or annotations.
+It is crucial to maintain any indentations or special characters present in the input, as the output will replace the original comment in the code editor.
+For example, if the input is a code comment, you must preserve the comment symbols and indentations in the output.
+Remember, the output should be formatted in a way that allows it to seamlessly replace the original text without requiring any further modifications.
+Additionally, do not add code block annotations like backticks, even if the input is a code block.
+
+The input is:
+${selectedText}
+`,
+					}],
+					model: openAiModelName as string,
+				})
+
+				console.log(response);
+
+				const result = response.choices[0].message.content;
+				if (result) {
+					editor.edit(editBuilder => {
+						editBuilder.replace(selection, result);
+					});
+				}
+			}
+			else {
+				vscode.window.showErrorMessage('No text selected');
+			}
+		} else {
+			vscode.window.showErrorMessage('No active text editor');
+		}
 	});
+
+
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
